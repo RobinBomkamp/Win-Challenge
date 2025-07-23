050af4b869cacb264464872b0c16b2b5975d3993
Robin Bomkamp <robin@bomkamp.com>
2025-07-24 01:34:38 +0200

    Add marvel rivals

--- src/lib/Viewer/Viewer.svelte
+++ src/lib/Viewer/Viewer.svelte
@@ -17,7 +17,8 @@
             { time: new Date(2025, 6, 23, 1, 0, 0), type: 'start'},
             { time: new Date(2025, 6, 23, 1, 20, 0), type: 'end'}
         ] },
-        { title: 'League of Legends', description: '1 Win im Ranked', times: [] }
+        { title: 'League of Legends', description: '1 Win im Ranked', times: [] },
+        { title: 'Marvel Rivals', description: '1 Win in einer normalen Runde', times: [] }
     ];
 </script>
 
